f1e8a58ed592ef80f2a5396f6ebd9916e2e0f324
rt-desai <abhyuday.desai@readytensor.com>
2025-11-16 22:02:10 -0800

    add lesson 1 script

---
+++ code/paths.py
@@ -0,0 +1,9 @@
+import os
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+CODE_DIR = os.path.join(ROOT_DIR, "code")
+
+DATA_DIR = os.path.join(ROOT_DIR, "data")
+
+OUTPUTS_DIR = os.path.join(ROOT_DIR, "outputs")
