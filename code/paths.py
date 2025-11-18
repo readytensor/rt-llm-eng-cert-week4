4a418f4455a5fed1d9a1f4401d5bc24395310e86
Mo <mohamedabdelhamid3030@gmail.com>
2025-11-18 21:28:59 +0200

    move outputs dir inside data dir

--- code/paths.py
+++ code/paths.py
@@ -1,4 +1,5 @@
 import os
+from sqlite3 import DatabaseError
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -6,7 +7,7 @@ CODE_DIR = os.path.join(ROOT_DIR, "code")
 
 DATA_DIR = os.path.join(ROOT_DIR, "data")
 
-OUTPUTS_DIR = os.path.join(ROOT_DIR, "outputs")
+OUTPUTS_DIR = os.path.join(DATA_DIR, "outputs")
 
 CONFIG_FILE_PATH = os.path.join(CODE_DIR, "config.yaml")
 
